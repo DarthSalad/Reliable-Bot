dc2f4feda48249aa5c3f598e5ae277897adc9bf8
Piyush Mishra <piyushmishra965@gmail.com>
2021-09-23 11:31:57 +0530

    working play function

--- src/cogs/music.py
+++ src/cogs/music.py
@@ -27,21 +27,45 @@ class Music(commands.Cog):
 
     @commands.command(pass_context=True)
     async def join(self, ctx):
-        print('test successful')
-        # if not ctx.author.voice:
-        #     await ctx.send("{} is not connected to a voice channel.".format(ctx.author.name))
-        #     return
-        # else:
-        #     channel = ctx.author.voice.channel
-        # await channel.connect()
+        # print('test successful')
+        if not ctx.author.voice:
+            await ctx.send("{} is not connected to a voice channel.".format(ctx.author.name))
+            return
+        else:
+            vc = ctx.author.voice.channel
+        player = self.bot.music.player_manager.create(ctx.guild.id, endpoint = str(ctx.guild.region))
+        if not player.is_connected:
+            player.store('channel', ctx.channel.id)
+            await self.connect_to(ctx.guild.id, str(vc.id))
 
     @commands.command(pass_context=True)
     async def leave(self, ctx):
         await ctx.voice_client.disconnect()
 
     @commands.command(pass_context=True)
-    async def play(self, ctx, url):
-        string = 'adsfkndsf'
+    async def play(self, ctx, *, query):
+        # join()
+        player = self.bot.music.player_manager.get(ctx.guild.id)
+        query = f'ytsearch:{query}'
+        results = await player.node.get_tracks(query)
+        tracks = results['tracks'][0:5]
+        i = 0
+        query_result = ''
+        for track in tracks:
+            i = i+1
+            query_result = query_result + f'{i}:  {track["info"]["title"]} - {track["info"]["uri"]}\n\n'
+        embed = Embed()
+        embed.description = query_result
+        await ctx.channel.send(embed=embed)
+
+        def check(m):
+            return m.author.id == ctx.author.id
+        response = await self.bot.wait_for('message', check=check)
+        track = tracks[int(response.content)-1]
+
+        player.add(requester = ctx.author.id, track = track)
+        if not player.is_playing:
+            await player.play()
 
 def setup(bot):
     bot.add_cog(Music(bot))
